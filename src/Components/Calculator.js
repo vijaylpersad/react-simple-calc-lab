9da9b0ab0e5442d5f64e14da0d6e010353726cb3
weston-bailey <wdbailey87@gmail.com>
2021-06-24 15:09:07 -0700

    update whitespace

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -3,8 +3,8 @@ import { useState } from 'react'
 export default function Calculator() {
   const [operator, setOperator] = useState('+')
   const [numbers, setNumbers] = useState({
-    num1: 0,
-    num2: 0
+  num1: 0,
+  num2: 0
   })
   const [result, setResult] = useState('')
 
@@ -22,23 +22,23 @@ export default function Calculator() {
 
 		switch (operator){
 			case '+':
-				setResult(int1 + int2)
+			  setResult(int1 + int2)
 				break
-        
+    
       case '-':
 				setResult(int1 - int2)
 				break
-        
+    
       case '*':
 				setResult(int1 * int2)
 				break
-        
+    
       case '/':
-          setResult(int1 / int2)
+         setResult(int1 / int2)
 				break
 			
 			default:
-        console.log(`${operator} is not a valid operator!`)
+    console.log(`${operator} is not a valid operator!`)
 		}
 	}
 
@@ -49,42 +49,42 @@ export default function Calculator() {
 	// }
 
   return (
-    <div className="container">
-      <h1>Add with React!</h1>
+  <div className="container">
+    <h1>Add with React!</h1>
 
-      <div className="add">
+    <div className="add">
 
-        <input 
-          type="number"
-          name="num1" 
-          placeholder="Enter your first number" 
-          value={numbers.num1}
-          onChange={ e => setNum(e, 'num1')}
-        />
+    <input 
+      type="number"
+      name="num1" 
+      placeholder="Enter your first number" 
+      value={numbers.num1}
+      onChange={ e => setNum(e, 'num1')}
+    />
 
-        <span>{operator}</span>
-        
-        <input 
-          type="number"
-          name="num2" 
-          placeholder="Enter your second number" 
-          value={numbers.num2}
-          onChange={ e => setNum(e, 'num2')}
-        />
+    <span>{operator}</span>
+    
+    <input 
+      type="number"
+      name="num2" 
+      placeholder="Enter your second number" 
+      value={numbers.num2}
+      onChange={ e => setNum(e, 'num2')}
+    />
 
-        <button onClick={ () => calculate(numbers.num1, numbers.num2) }>=</button>
+    <button onClick={ () => calculate(numbers.num1, numbers.num2) }>=</button>
 
-        <h3>{result}</h3>
+    <h3>{result}</h3>
 
-        <button name="plus" value="+" onClick={e => setOperator(e.target.value)}>+</button>
+    <button name="plus" value="+" onClick={e => setOperator(e.target.value)}>+</button>
 
-        <button name="sub" value="-" onClick={e => setOperator(e.target.value)}>-</button>
+    <button name="sub" value="-" onClick={e => setOperator(e.target.value)}>-</button>
 
-        <button name="mult" value="*" onClick={e => setOperator(e.target.value)}>*</button>
+    <button name="mult" value="*" onClick={e => setOperator(e.target.value)}>*</button>
 
-        <button name="div" value="/" onClick={e => setOperator(e.target.value)}>/</button>
-      </div>
+    <button name="div" value="/" onClick={e => setOperator(e.target.value)}>/</button>
     </div>
+  </div>
   )
 
 }
