5635f8bef2e2fff4b5d6027aec9080cacb7eb19b
weston-bailey <wdbailey87@gmail.com>
2021-06-24 15:11:49 -0700

    format with prettier

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,90 +1,111 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 export default function Calculator() {
-  const [operator, setOperator] = useState('+')
+  const [operator, setOperator] = useState("+")
   const [numbers, setNumbers] = useState({
-  num1: 0,
-  num2: 0
+    num1: 0,
+    num2: 0,
   })
-  const [result, setResult] = useState('')
+  const [result, setResult] = useState("")
 
-	const setNum = (e, num) => {
-  setNumbers({ 
-    ...numbers,  
-    [num]: e.target.value
-  })
-	}
+  const setNum = (e, num) => {
+    setNumbers({
+      ...numbers,
+      [num]: e.target.value,
+    })
+  }
 
-	const calculate = (num1, num2) => {
+  const calculate = (num1, num2) => {
+    const int1 = Number(num1)
+    const int2 = Number(num2)
 
-		const int1 = Number(num1)
-		const int2 = Number(num2)
+    switch (operator) {
+      case "+":
+        setResult(int1 + int2)
+        break
 
-		switch (operator){
-		  case '+':
-			  setResult(int1 + int2)
-				break
-    
-      case '-':
-				setResult(int1 - int2)
-				break
-    
-      case '*':
-				setResult(int1 * int2)
-				break
-    
-      case '/':
-         setResult(int1 / int2)
-				break
-			
-			default:
+      case "-":
+        setResult(int1 - int2)
+        break
+
+      case "*":
+        setResult(int1 * int2)
+        break
+
+      case "/":
+        setResult(int1 / int2)
+        break
+
+      default:
         console.log(`${operator} is not a valid operator!`)
-		}
-	}
+    }
+  }
 
-	// const setOp = (e) => {
-	// 	this.setState({
-	// 		operator: e.target.value
-	// 	})
-	// }
+  // const setOp = (e) => {
+  // 	this.setState({
+  // 		operator: e.target.value
+  // 	})
+  // }
 
   return (
-  <div className="container">
-  <h1>Add with React!</h1>
+    <div className="container">
+      <h1>Add with React!</h1>
 
-  <div className="add">
+      <div className="add">
+        <input
+          type="number"
+          name="num1"
+          placeholder="Enter your first number"
+          value={numbers.num1}
+          onChange={(e) => setNum(e, "num1")}
+        />
 
-  <input 
-    type="number"
-    name="num1" 
-    placeholder="Enter your first number" 
-    value={numbers.num1}
-    onChange={ e => setNum(e, 'num1')}
-  />
+        <span>{operator}</span>
 
-  <span>{operator}</span>
-  
-  <input 
-    type="number"
-    name="num2" 
-    placeholder="Enter your second number" 
-    value={numbers.num2}
-    onChange={ e => setNum(e, 'num2')}
-  />
+        <input
+          type="number"
+          name="num2"
+          placeholder="Enter your second number"
+          value={numbers.num2}
+          onChange={(e) => setNum(e, "num2")}
+        />
 
-  <button onClick={ () => calculate(numbers.num1, numbers.num2) }>=</button>
+        <button onClick={() => calculate(numbers.num1, numbers.num2)}>=</button>
 
-  <h3>{result}</h3>
+        <h3>{result}</h3>
 
-  <button name="plus" value="+" onClick={e => setOperator(e.target.value)}>+</button>
+        <button
+          name="plus"
+          value="+"
+          onClick={(e) => setOperator(e.target.value)}
+        >
+          +
+        </button>
 
-  <button name="sub" value="-" onClick={e => setOperator(e.target.value)}>-</button>
+        <button
+          name="sub"
+          value="-"
+          onClick={(e) => setOperator(e.target.value)}
+        >
+          -
+        </button>
 
-  <button name="mult" value="*" onClick={e => setOperator(e.target.value)}>*</button>
+        <button
+          name="mult"
+          value="*"
+          onClick={(e) => setOperator(e.target.value)}
+        >
+          *
+        </button>
 
-  <button name="div" value="/" onClick={e => setOperator(e.target.value)}>/</button>
-  </div>
-  </div>
+        <button
+          name="div"
+          value="/"
+          onClick={(e) => setOperator(e.target.value)}
+        >
+          /
+        </button>
+      </div>
+    </div>
   )
-
 }
