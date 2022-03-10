8fc5a08ee93790e8b0f97bf6675542a9e26ae457
vijaylpersad <vijaylpersad@utexas.edu>
2022-03-10 12:15:58 -0600

    working calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 
 class Calculator extends Component {
-    state = {  } 
+    state = { 
+        num1: 0,
+        num2: 0,
+        sum: 0
+     } 
+
+    setNum = (e) => {
+       // this.setState({[num]: e.target.value}) name specified in form
+       this.setState({[e.target.name]: parseInt(e.target.value)}) //parse so it doesnt do 10+10 = 1010
+    }
+
+    calculate = (e) => {
+        e.preventDefault()
+
+        this.setState({ 
+            sum: this.state.num1 +this.state.num2
+         })
+    }
+
     render() { 
         return (
-            <div className="container">
+            <div className="caclulator">
                 <h1>Add with React!</h1>
 
-                <div className="add">
-                    <input type="text" />
+                <form onSubmit={this.calculate}>
+                    <input type="number" 
+                        name="num1"
+                        value={this.state.num1}
+                        onChange={this.setNum}
+                    />
                     <span>+</span>
-                    <input type="text" />
+                    <input type="number" 
+                        name="num2"
+                        value={this.state.num2}
+                        onChange={this.setNum}
+                    />
                     <button>=</button>
-                    <h3>Addition results go here!</h3>
-                </div>
+                    <h3>{this.state.sum}</h3>
+                </form>
             </div>
         );
     }
